574a23f09d35cfa9c4c8cf55b6df290a535750ce
BDex0tED <bemanaff@gmail.com>
2026-02-21 22:06:52 +0600

    Initial commit

---
+++ src/main/java/com/tradeops/TradeOpsApplication.java
@@ -0,0 +1,13 @@
+package com.tradeops;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TradeOpsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TradeOpsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/tradeops/TradeOpsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tradeops;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TradeOpsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
